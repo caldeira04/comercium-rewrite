4e0ee172e77e043fb2bb9da82a174c3425ab2363
caldeira04 <joao.p.caldeira@icloud.com>
2026-05-20 00:17:45 -0300

    [sale]add files

--- backend/src/db/schema/sale.ts
+++ backend/src/db/schema/sale.ts
@@ -1,6 +1,41 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { generateUniqueString } from "@/utils/general"
+import { relations } from "drizzle-orm";
+import { product } from "./product";
+import { timestamps } from "@/utils/drizzle";
+import { client } from "./client";
 
-const sale = sqliteTable("sale", {
-    id: text("id").primaryKey().$default(() => generateUniqueString(24))
+export const sale = sqliteTable("sale", {
+    id: text("id").primaryKey().$default(() => generateUniqueString(24)),
+    totalAmount: integer("total_amount").notNull().default(0),
+    clientId: integer("client_id").notNull().references(() => client.id, { onDelete: "restrict" }),
+    settledAt: text("settled_at"),
+    cancelledAt: text("cancelled_at"),
+    ...timestamps
 })
+
+export const saleRelations = relations(sale, ({ many }) => ({
+    saleItem: many(saleItem)
+}))
+
+export const saleItem = sqliteTable("sale_items", {
+    id: text("id").primaryKey().$default(() => generateUniqueString(24)),
+    saleId: text("id").notNull().references(() => sale.id, { onDelete: "cascade" }),
+    productId: integer("product_id").notNull().references(() => product.id, { onDelete: "restrict" }),
+    quantity: integer("quantity").notNull(),
+    unitPrice: integer("unit_price").notNull(),
+    totalPrice: integer("total_price").notNull(),
+    ...timestamps
+})
+
+export const saleItemRelations = relations(saleItem, ({ one }) => ({
+    sale: one(sale, {
+        fields: [saleItem.saleId],
+        references: [sale.id]
+    }),
+
+    product: one(product, {
+        fields: [saleItem.productId],
+        references: [product.id]
+    }),
+}))
